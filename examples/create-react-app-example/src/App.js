0fdb2ea256c6ded75bf69602ea86b5450a40b5e7
Hakim Hauston <hakim.hauston@storms.com>
2022-01-19 09:28:23 +0800

    found the cause of the problem, server cannot run ngrok, the client can reach it but will get cors problem probably because of the redirection done by ngrok

--- examples/create-react-app-example/src/App.js
+++ examples/create-react-app-example/src/App.js
@@ -2,7 +2,11 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import io from 'socket.io-client';
 
-const socket = io('localhost:3001');
+// const socket = io('localhost:3001');
+// const socket = io('https://6660-13-250-193-173.ngrok.io', {cors: true});
+// const socket = io('https://burnt-vivid-plow.glitch.me');
+// const socket = io('https://3c3b-13-250-193-173.ngrok.io');
+const socket = io('http://localhost:3001');
 
 function App() {
   const [isConnected, setIsConnected] = useState(socket.connected);
